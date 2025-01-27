902c8674ec500dcce39e7cae6665b9c8ab7efa9c
ivymmurithi <ivymurithi@gmail.com>
2025-01-27 12:25:49 +0300

    switch, function return variables, refactor

--- hello_test.go
+++ hello_test.go
@@ -5,14 +5,20 @@ import "testing"
 func TestHello(t *testing.T) {
 	//subtests --> testing a thing and then describing different scenarios
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
-	
+
 	t.Run("say 'Hello, World' when an empty string is passed", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Ivy", "Spanish")
+		want := "Hola, Ivy"
 		assertCorrectMessage(t, got, want)
 	})
 }
