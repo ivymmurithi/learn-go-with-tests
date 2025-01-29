d7146908484f6eefe9febbc154b43ffa6d152dd5
ivymmurithi <ivymurithi@gmail.com>
2025-01-29 09:39:01 +0300

    structs, methods, interfaces

---
+++ structs/structs.go
@@ -0,0 +1,41 @@
+package structs
+
+import "math"
+
+// if the type  ypu pass in matches what the interface is asking for(a method Area() and float64 return) then it will compile
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64{
+	return t.Base * t.Height * 0.5
+}
+
+// make functions that can be used with diff types and still maintain type safety
